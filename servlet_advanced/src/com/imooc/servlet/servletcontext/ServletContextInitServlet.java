af8ff1bf444d770f053506f3380560dc3f2f5769
PeiroJack <3532926353@qq.com>
2020-03-15 14:49:47 +0800

    learn ServletContext

---
+++ servlet_advanced/src/com/imooc/servlet/servletcontext/ServletContextInitServlet.java
@@ -0,0 +1,38 @@
+package com.imooc.servlet.servletcontext;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletContextInitServlet
+ */
+@WebServlet("/servletcontext/index")
+public class ServletContextInitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletContextInitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext context = request.getServletContext();
+		context.setAttribute("copyright", "@2018 imooc");
+		context.setAttribute("title", "Ä½¿ÎÍø");
+		response.getWriter().println("init success");
+	}
+
+}
